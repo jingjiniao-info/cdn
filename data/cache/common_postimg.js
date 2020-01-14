3357c1bbba1fef39d1228791eb66dc20f4e7b0df
jingjiniao <feng.jingjiniao@gmail.com>
2020-01-15 05:29:13 +0800

    '第一次提交'

---
+++ data/cache/common_postimg.js
@@ -0,0 +1,1 @@
+var postimg_type = new Array();postimg_type["hrline"]=["2.gif","line1.png","4.gif","line4.png","line9.png","line2.png","3.gif","0.gif","line6.png","line8.png","line3.png","line7.png","5.gif","line5.png","1.gif"];postimg_type["postbg"]=["bg9.png","bg10.png","bg1.png","2.jpg","bg7.png","bg2.png","0.gif","bg4.png","bg3.png","bg6.png","1.jpg","3.jpg","bg5.png","bg8.png"];
